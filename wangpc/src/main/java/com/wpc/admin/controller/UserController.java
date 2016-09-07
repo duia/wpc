8ae91e177e94d77df50b82fc62a5843d46caf045
王鹏程 <wpcfree@qq.com>
2016-09-07 23:24:34 +0800

    ul

--- wangpc/src/main/java/com/wpc/admin/controller/UserController.java
+++ wangpc/src/main/java/com/wpc/admin/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -38,7 +39,99 @@ public class UserController {
 	
 	@RequestMapping(value="/search", method=RequestMethod.POST)
 	@ResponseBody
-	public DataTableResult<User> search(ModelMap model, Integer draw, Integer start, Integer length) {
+	public DataTableResult<User> search(ModelMap model, HttpServletRequest request) {
+		
+		Integer orderColumn = Integer.valueOf(request.getParameter("order[0][column]"));
+	    System.out.println("orderColumn:"+orderColumn);
+	    //orderColumn = cols[Integer.parseInt(orderColumn)];
+		
+	    //获取Datatables发送的参数 必要
+		Integer draw = Integer.valueOf(request.getParameter("draw"));//这个值作者会直接返回给前台
+		System.out.println("draw:"+draw);
+		//排序
+		//String orderColumn = baseQuery.getOrder().get(0).getColumn();//[0]column'];//那一列排序，从0开始
+		//$order_dir = $_GET['order']['0']['dir'];//ase desc 升序或者降序
+		 
+//		//拼接排序sql
+//		$orderSql = "";
+//		if(isset($order_column)){
+//		    $i = intval($order_column);
+//		    switch($i){
+//		        case 0;$orderSql = " order by first_name ".$order_dir;break;
+//		        case 1;$orderSql = " order by last_name ".$order_dir;break;
+//		        case 2;$orderSql = " order by position ".$order_dir;break;
+//		        case 3;$orderSql = " order by office ".$order_dir;break;
+//		        case 4;$orderSql = " order by start_date ".$order_dir;break;
+//		        case 5;$orderSql = " order by salary ".$order_dir;break;
+//		        default;$orderSql = '';
+//		    }
+//		}
+//		//搜索
+//		$search = $_GET['search']['value'];//获取前台传过来的过滤条件
+//		 
+//		//分页
+//		$start = $_GET['start'];//从多少开始
+		Integer length = Integer.valueOf(request.getParameter("length"));
+		System.out.println("length:"+length);
+//		$length = $_GET['length'];//数据长度
+//		$limitSql = '';
+//		$limitFlag = isset($_GET['start']) && $length != -1 ;
+//		if ($limitFlag ) {
+//		    $limitSql = " LIMIT ".intval($start).", ".intval($length);
+//		}
+//		 
+//		//定义查询数据总记录数sql
+//		$sumSql = "SELECT count(id) as sum FROM DATATABLES_DEMO";
+//		//条件过滤后记录数 必要
+//		$recordsFiltered = 0;
+//		//表的总记录数 必要
+//		$recordsTotal = 0;
+//		$recordsTotalResult = $db->query($sumSql);
+//		while ($row = $recordsTotalResult->fetchArray(SQLITE3_ASSOC)) {
+//		    $recordsTotal =  $row['sum'];
+//		}
+//		//定义过滤条件查询过滤后的记录数sql
+//		$sumSqlWhere =" where first_name||last_name||position||office||start_date||salary LIKE '%".$search."%'";
+//		if(strlen($search)>0){
+//		    $recordsFilteredResult = $db->query($sumSql.$sumSqlWhere);
+//		    while ($row = $recordsFilteredResult->fetchArray(SQLITE3_ASSOC)) {
+//		        $recordsFiltered =  $row['sum'];
+//		    }
+//		}else{
+//		    $recordsFiltered = $recordsTotal;
+//		}
+//		 
+//		//query data
+//		$totalResultSql = "SELECT first_name,last_name,position,office,start_date,salary FROM DATATABLES_DEMO";
+//		$infos = array();
+//		if(strlen($search)>0){
+//		    //如果有搜索条件，按条件过滤找出记录
+//		    $dataResult = $db->query($totalResultSql.$sumSqlWhere.$orderSql.$limitSql);
+//		    while ($row = $dataResult->fetchArray(SQLITE3_ASSOC)) {
+//		        $obj = array($row['first_name'], $row['last_name'], $row['position'], $row['office'], $row['start_date'], $row['salary']);
+//		        array_push($infos,$obj);
+//		    }
+//		}else{
+//		    //直接查询所有记录
+//		    $dataResult = $db->query($totalResultSql.$orderSql.$limitSql);
+//		    while ($row = $dataResult->fetchArray(SQLITE3_ASSOC)) {
+//		        $obj = array($row['first_name'], $row['last_name'], $row['position'],$row['office'], $row['start_date'], $row['salary']);
+//		        array_push($infos,$obj);
+//		    }
+//		}
+//		 
+//		/*
+//		 * Output 包含的是必要的
+//		 */
+//		echo json_encode(array(
+//		    "draw" => intval($draw),
+//		    "recordsTotal" => intval($recordsTotal),
+//		    "recordsFiltered" => intval($recordsFiltered),
+//		    "data" => $infos
+//		),JSON_UNESCAPED_UNICODE);
+//		 
+		 
+		
 		DataTableResult<User> dtr = new DataTableResult<User>();
 		dtr.setDraw(draw);
 		dtr.setRecordsTotal(57);
---
+++ wangpc/src/main/java/com/wpc/common/Order.java
@@ -0,0 +1,18 @@
+package com.wpc.common;
+
+public class Order {
+	private String column;
+	private String dir;
+	public String getColumn() {
+		return column;
+	}
+	public void setColumn(String column) {
+		this.column = column;
+	}
+	public String getDir() {
+		return dir;
+	}
+	public void setDir(String dir) {
+		this.dir = dir;
+	}
+}
