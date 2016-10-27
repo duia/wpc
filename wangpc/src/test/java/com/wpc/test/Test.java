8e2a069a7287288ef03e36c88d6829e7b9576467
王鹏程 <376205421@qq.com>
2016-10-27 14:09:03 +0800

    test

--- wangpc/src/test/java/com/wpc/test/Test.java
+++ wangpc/src/test/java/com/wpc/test/Test.java
@@ -1,10 +1,17 @@
 package com.wpc.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.wpc.admin.entity.User;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ApplicationContext ac = new ClassPathXmlApplicationContext("classpath*:config/applicationContextTest.xml");
+		User user = (User) ac.getBean("a");
+		System.out.println(user);
 	}
 
 }
